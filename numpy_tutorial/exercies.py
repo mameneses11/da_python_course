05471722c4b8562697fc855ce290f4630340c947
Miguel Angel Meneses Alvarez <mamalvarez11@gmail.com>
2021-10-14 14:08:10 -0500

    Add numpy tutorial

---
+++ numpy_tutorial/exercies.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+np.zeros(10)
+
+np.ones(10)
+
+np.ones(10) * 5
+
+np.arange(10, 51)
+
+np.arange(10, 51, 2)
+
+np.arange(9).reshape((3,3))
+
+np.eye(3)
+
+np.random.rand(1)
+
+np.random.randn(25)
+
+np.arange(1, 101).reshape((10, 10)) / 100
+
+np.linspace(0, 1, 20)
+
+mat = np.arange(1,26).reshape(5,5)
+
+mat[2:,1:]
+
+mat[-2,-1]
+
+mat[:3, 1:2]
+
+mat[-1]
+
+mat[-2:]
+
+np.sum(mat)
+
+np.std(mat)
+
+np.sum(mat, 0)
